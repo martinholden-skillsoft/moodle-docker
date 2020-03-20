3ca095b9188e356fb3954419b89b0d584966c1e3
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2020-03-20 13:18:18 +0000

    Added cmd line params
    Added options to specify the files to process - this is just the filename it should always be in root of html folder

--- bin/start_uploadpage_cli.cmd
+++ bin/start_uploadpage_cli.cmd
@@ -1,12 +1,18 @@
 @ECHO OFF
+:: the first parameter is an optional filename
+IF "%1"=="" ( SET "MOODLE_UPLOAD_FILE=moodle_channels.csv" ) ELSE ( SET "MOODLE_UPLOAD_FILE=%1" )
+:: the second parameter is an optional categoryid
+IF "%2"=="" ( SET "MOODLE_UPLOAD_CATEGORYID=1" ) ELSE ( SET "MOODLE_UPLOAD_CATEGORYID=%1" )
 
 PUSHD %cd%
 CD %~dp0..
 SET BASEDIR=%cd%
 POPD
 
+
+
 echo.
 echo *** Run the Moodle CLI script: admin/tool/uploadpage/cli/uploadpage.php
 echo.
-call %BASEDIR%\bin\moodle-docker-compose exec webserver php admin/tool/uploadpage/cli/uploadpage.php --source=../../../../moodle_channels.csv --categoryid=1
+call %BASEDIR%\bin\moodle-docker-compose exec webserver php admin/tool/uploadpage/cli/uploadpage.php --source=../../../../%MOODLE_UPLOAD_FILE% --categoryid=%MOODLE_UPLOAD_CATEGORYID%
 echo.
--- bin/start_uploadpagecompletions_cli.cmd
+++ bin/start_uploadpagecompletions_cli.cmd
@@ -1,4 +1,6 @@
 @ECHO OFF
+:: the first parameter is an optional filename
+IF "%1"=="" ( SET "MOODLE_UPLOAD_COMPLETIONFILE=moodle_results.csv" ) ELSE ( SET "MOODLE_UPLOAD_COMPLETIONFILE=%1" )
 
 PUSHD %cd%
 CD %~dp0..
@@ -8,5 +10,5 @@ POPD
 echo.
 echo *** Run the Moodle CLI script: admin/tool/uploadpagecompletions/cli/uploadpagecompletions.php
 echo.
-call %BASEDIR%\bin\moodle-docker-compose exec webserver php admin/tool/uploadpagecompletions/cli/uploadpagecompletions.php --source=../../../../moodle_results.csv
+call %BASEDIR%\bin\moodle-docker-compose exec webserver php admin/tool/uploadpagecompletions/cli/uploadpagecompletions.php --source=../../../../%MOODLE_UPLOAD_COMPLETIONFILE%
 echo.
