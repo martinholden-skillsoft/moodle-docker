77979ebeae0a2ef8ba44c53ed9fa66815f4291f2
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2020-03-20 11:45:39 +0000

    Updates to pgadmin4 and cmds

--- bin/start_moodle.cmd
+++ bin/start_moodle.cmd
@@ -1,5 +1,9 @@
 @ECHO OFF
 
+@ECHO OFF
+:: the first parameter is an optional version
+IF "%1"=="" ( SET "MOODLE_DOCKER_PHP_VERSION=7.2" ) ELSE ( SET "MOODLE_DOCKER_PHP_VERSION=%1" )
+
 PUSHD %cd%
 CD %~dp0..
 SET BASEDIR=%cd%
@@ -22,3 +26,14 @@ echo *** Moodle is running please. Browse to - http://%MOODLE_DOCKER_WEB_PORT%
 echo *** Moodle Admin Username: admin
 echo *** Moodle Admin password: test
 echo.
+
+IF "%MOODLE_DOCKER_DB%"=="pgsql" (
+	echo.
+	echo *** pgAdmin4 is running please. Browse to - http://127.0.0.1:5050
+	echo *** pgAdmin4 User: user@domain.com
+	echo *** pgAdmin4 Password: SuperSecret
+	echo.
+	echo *** Moodle Database Username: moodle
+	echo *** Moodle Database Username: m@0dl3ing
+	echo.
+)
