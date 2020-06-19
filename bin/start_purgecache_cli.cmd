09bc4a3e9013a521a2356cc3f195cc4749d240c0
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2020-06-19 15:03:50 +0100

    Enhancements

---
+++ bin/start_purgecache_cli.cmd
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+PUSHD %cd%
+CD %~dp0..
+SET BASEDIR=%cd%
+POPD
+
+echo.
+echo *** Run the Moodle CLI script: admin/cli/purge_caches.php
+echo.
+call %BASEDIR%\bin\moodle-docker-compose exec webserver php admin/cli/purge_caches.php
+echo.
