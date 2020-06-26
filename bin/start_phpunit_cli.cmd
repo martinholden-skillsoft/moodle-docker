a855815a87020ccedd97a0680ef9637d1a6470a2
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2020-06-26 15:44:00 +0100

    Support passing phpunit cmdline

--- bin/start_phpunit_cli.cmd
+++ bin/start_phpunit_cli.cmd
@@ -25,12 +25,17 @@ CD %~dp0..
 SET BASEDIR=%cd%
 POPD
 
+:: Get the rest of the optional command line after first two arguments
+SHIFT
+SHIFT
+SET PHPUNITOPTIONS=%1
+
 echo.
 echo *** Moodle CLI Script: admin/tool/phpunit/cli/init.php
 call %BASEDIR%\bin\moodle-docker-compose exec webserver php admin/tool/phpunit/cli/init.php
 echo.
 
 echo.
-echo *** Moodle CLI Script: vendor/bin/phpunit %PHPUNIT_TESTCASE% %PHPUNIT_PATH%
-call %BASEDIR%\bin\moodle-docker-compose exec webserver vendor/bin/phpunit %PHPUNIT_TESTCASE% %PHPUNIT_PATH%
+echo *** Moodle CLI Script: vendor/bin/phpunit %PHPUNITOPTIONS% %PHPUNIT_TESTCASE% %PHPUNIT_PATH%
+call %BASEDIR%\bin\moodle-docker-compose exec webserver vendor/bin/phpunit %PHPUNITOPTIONS% %PHPUNIT_TESTCASE% %PHPUNIT_PATH%
 echo.
